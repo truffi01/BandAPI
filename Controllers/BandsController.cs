366b1deb8dd2517748a9df29f00006f1faabe8ff
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-26 16:53:37 -0800

    Enter API Get Request for Bands

--- Controllers/BandsController.cs
+++ Controllers/BandsController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 namespace BandAPI.Controllers
 {
     [ApiController]
+    [Route("api/bands")]
     public class BandsController: ControllerBase
     {
         private readonly IBandAlbumRepository _bandAlbumRepository;
@@ -18,12 +19,19 @@ namespace BandAPI.Controllers
                 throw new ArgumentException(nameof(bandAlbumRepository));
         }
 
-        [HttpGet("api/bands")]
+        [HttpGet]
         public IActionResult GetBands()
         {
             var bandsFromRepo = _bandAlbumRepository.GetBands();
             return new JsonResult(bandsFromRepo);
 
+        }
+
+        [HttpGet("{bandId}")]
+        public IActionResult GetBand(Guid bandId)
+        {
+            var bandFromRepo = _bandAlbumRepository.GetBand(bandId);
+            return new JsonResult(bandFromRepo);
         }
 
     }
